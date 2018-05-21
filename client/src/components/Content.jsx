91d3ee0b41a2a95f559c9847d6bfee248d37017c
sravsk <sravanthi.karanam@gmail.com>
2018-05-20 18:20:31 -0700

    fixed gitignore bug and added basic components

---
+++ client/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Content = (props) => {
+  return (
+    <div>
+      <div className="wine-block">Find a Wine</div>
+      <div className="movie-block">Find a Movie</div>
+    </div>
+    )
+}
+
+export default Content;
