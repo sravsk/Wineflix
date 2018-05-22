9a781a004e5c44360a3d35f0aa30539483995a61
Neiman Chen <hahatuke@gmail.com>
2018-05-21 23:16:36 -0700

    added movies button

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Wines from './Wines.jsx';
+import Movies from './Movies.jsx';
 import { Grid } from 'react-bootstrap';
 
 const Content = (props) => {
   return (
     <div>
       <Grid>
-        <Wines/>
-        <div className="movie-block">Find a Movie</div>
+        <Wines />
+        <Movies />
       </Grid>
     </div>
     )
