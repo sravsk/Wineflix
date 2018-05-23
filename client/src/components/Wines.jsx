aa8a94d5f0f8c3fb6295d530452a6be640cadf82
Neiman Chen <hahatuke@gmail.com>
2018-05-22 20:28:23 -0700

    commit

--- client/src/components/Wines.jsx
+++ client/src/components/Wines.jsx
@@ -4,9 +4,9 @@ import { Button } from 'react-bootstrap';
 const Wines = (props) => {
   return(
     <div className="wine-block">
-      <Button className="fnd-wine-button"><span>Fine a Wine</span></Button>
+      <Button className="fnd-wine-button"><span>Find a Wine</span></Button>
     </div>
-    )
+  )
 }
 
 export default Wines;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,8 @@ import $ from 'jquery';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Content from './components/Content.jsx';
-import css from './styles/style.css';
+import MoviesPage from './components/MoviesPage.jsx';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -16,9 +17,10 @@ class App extends React.Component {
       <div>
         <div className="header"><Header/></div>
         <div className="content"><Content/></div>
+        <MoviesPage />
         <div className="footer"><Footer/></div>
       </div>
-      )
+    )
   }
 }
 
