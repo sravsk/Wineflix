90112235e3e157d809f7123cb67f20f1ef1bebce
Neiman Chen <hahatuke@gmail.com>
2018-05-23 16:59:10 -0700

    fetches some data from moviedb api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const request = require('request');
 const dotenv = require('dotenv');
 const path = require('path');
 
@@ -11,6 +11,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(express.static(__dirname + '/../client/dist'));
 
+
+let getMovieData = () => {
+
+  var options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/discover/movie',
+    qs:
+     { page: '1',
+       include_video: 'false',
+       include_adult: 'false',
+       sort_by: 'popularity.desc',
+       language: 'en-US',
+       api_key: '6f0f6ccafaaff428439efd8e3edd254a'
+     },
+    body: '{}' };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(JSON.parse(body));
+  });
+
+}
+
+getMovieData();
 
 
 
