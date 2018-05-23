00df5347d5f268939d5372c5a9212357b0c7c83c
sravsk <sravanthi.karanam@gmail.com>
2018-05-23 16:03:32 -0700

    render wines sample data

--- client/src/components/Wines.jsx
+++ client/src/components/Wines.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
+import PopulateWines from './PopulateWines.jsx';
+import SearchWines from './SearchWines.jsx';
+import SidebarWines from './SidebarWines.jsx';
+import winesData from '../data/winesData.js';
 
-const Wines = (props) => {
+class Wines extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      wines : []
+    }
+  }
+
+  render() {
   return(
-    <div className="searchbox">
-
-    </div>
-  )
+    <Grid>
+      <SearchWines/>
+      {/*<SidebarWines wines={winesData}/>*/}
+      <PopulateWines wines={winesData}/>
+    </Grid>
+    )
+ }
 }
-
 export default Wines;
