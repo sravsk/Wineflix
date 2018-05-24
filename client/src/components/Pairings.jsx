00f55743cffa07135f4eac83328256b21af62cf8
Zoey Ikemoto <zmi384@gmail.com>
2018-05-24 16:00:19 -0700

    added Pairings component to front page

--- client/src/components/Pairings.jsx
+++ client/src/components/Pairings.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Grid } from 'react-bootstrap';
-import PopulateMovies from './PopulateMovies.jsx';
-import SearchMovies from './SearchMovies.jsx';
+import PopulatePairings from './PopulatePairings.jsx';
 //import SidebarMovies from './SidebarMovies.jsx';
 import moviesData from '../data/moviesData.js';
 
@@ -18,9 +17,8 @@ class Pairings extends React.Component {
     return(
       <Grid>
       <div>hi there</div>
-        <SearchMovies/>
         {/*<SidebarMovies movies={moviesData}/>*/}
-        <PopulateMovies movies={moviesData}/>
+        <PopulatePairings movies={moviesData}/>
       </Grid>
       )
   }
