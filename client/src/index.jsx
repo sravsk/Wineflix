109014bbd6fdda82db1e58700830375d23bddd97
Zoey Ikemoto <zmi384@gmail.com>
2018-05-24 16:00:19 -0700

    added Pairings component to front page

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer.jsx';
 import Content from './components/Content.jsx';
 import Wines from './components/Wines.jsx';
 import Movies from './components/Movies.jsx';
+import Pairings from './components/Pairings.jsx';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import css from './styles/style.css';
 
@@ -26,6 +27,7 @@ class App extends React.Component {
               <Route path="/" exact={true} component={Content}/>
               <Route path="/wines" exact={true} component={Wines}/>
               <Route path="/movies" exact={true} component={Movies}/>
+              <Route path="/pairings" exact={true} component={Pairings}/>
            </Switch>
           </Router>
           <div className="footer"><Footer/></div>
