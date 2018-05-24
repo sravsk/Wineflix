5baae7aba73e7f20324b76b9a1a1d2fb6ef2783b
Neiman Chen <hahatuke@gmail.com>
2018-05-24 11:02:41 -0700

    git troubles

--- client/src/components/Movies.jsx
+++ client/src/components/Movies.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-<<<<<<< HEAD
 import { Grid } from 'react-bootstrap';
 import PopulateMovies from './PopulateMovies.jsx';
 import SearchMovies from './SearchMovies.jsx';
@@ -24,15 +23,6 @@ class Movies extends React.Component {
       </Grid>
       )
   }
-=======
-
-const Movies = (props) => {
-  return(
-    <div className="movie-block">
-      <Button className="fnd-movie-button"><span>Find a Movie</span></Button>
-    </div>
-  )
->>>>>>> 6aa55950cda16f949067dba2fd7bc8379785e3a1
 }
 
 export default Movies;
