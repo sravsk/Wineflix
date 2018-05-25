602a2393e211c4fa319d96d671e9d9f451f2897a
Zoey Ikemoto <zmi384@gmail.com>
2018-05-24 17:56:55 -0700

    wine data now populates dynamically

--- client/src/components/PopulatePairings.jsx
+++ client/src/components/PopulatePairings.jsx
@@ -5,26 +5,30 @@ class PopulatePairings extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      data : this.props.movies
-    }
+      data : this.props.movies,
+      wines : this.props.wines,
+    };
   }
 
   render(){
     return(
       <Grid>
-        {this.state.data.map((movie) => (
+        {this.state.data.map((movie, index) => (
           <Row key={movie.id}>
             <div>
                <Col xs={6} md={4}>
                   <Thumbnail src={`http://image.tmdb.org/t/p/original${movie.poster_path}`}/>
                 </Col>
-                <Col xs={6} md={4}>
-                  <div>{movie.title}</div>
-                  <Button>Suggest Wines</Button>
+               <Col xs={3} md={3}>
+                  <div class="pairing-info">
+                    <h4>{movie.title}</h4>
+                    <p>paired with:</p>
+                    <h4>{this.state.wines[index].name}</h4>
+                    <h1>&#x1f44d;   &#x1f44e;</h1>
+                  </div>
                 </Col>
-                <Col xs={6} md={4}>
-                  <div>Rating: {movie.vote_average}</div>
-
+                <Col xs={3} md={3}>
+                  <Thumbnail src={this.state.wines[index].image_thumb_url}/>
                 </Col>
                 <Col xs={6} md={4}>
                   <div>Votes: {movie.vote_count}</div>
