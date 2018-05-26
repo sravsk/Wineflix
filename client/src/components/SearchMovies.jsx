b724088f7cc4b3e5aa4cfde60e26675bc2eee04d
Neiman Chen <hahatuke@gmail.com>
2018-05-25 22:24:57 -0700

    refactor into class component

--- client/src/components/SearchMovies.jsx
+++ client/src/components/SearchMovies.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Grid, Row, Button} from 'react-bootstrap';
 
-const SearchMovies = () => {
-  return(
-    <Grid>
-      <Row>
-        <div className="search-wrapper">
-          <input className="input-search" placeholder="search movies"/>
-        </div>
-      </Row>
-    </Grid>
+class SearchMovies extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  render() {
+    return(
+      <Grid>
+        <Row>
+          <div className="search-wrapper">
+            <input className="input-search" placeholder="search movies"/>
+          </div>
+        </Row>
+      </Grid>
     )
   }
+}
 
 export default SearchMovies;
