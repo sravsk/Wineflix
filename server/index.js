099d4614b8537cbf054769f1e6fb3910a56a362e
Neiman Chen <hahatuke@gmail.com>
2018-05-27 12:30:13 -0700

    created new endpoint to get movies clientside

--- server/index.js
+++ server/index.js
@@ -40,11 +40,18 @@ app.get('/api/analyzeWines', (req, res, next) => {
 });
 
 
-app.get('/moviesFetcher', (req, res) => {
+app.get('/api/moviesFetcher', (req, res) => {
   movieService.fetchMovies((data, cb) => {
     if (cb) {
       res.json("movies data sent")
     }
+  })
+})
+
+app.get('/moviedata', (req, res) => {
+  db.getMovies((movies) => {
+    console.log('movies', movies)
+    res.send(movies);
   })
 })
 
