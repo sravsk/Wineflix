4cea2330170a0f97e6a80ad99406714226616058
Neiman Chen <hahatuke@gmail.com>
2018-05-27 11:17:16 -0700

    WIP handling keyup

--- client/src/components/SearchMovies.jsx
+++ client/src/components/SearchMovies.jsx
@@ -9,12 +9,22 @@ class SearchMovies extends React.Component {
     }
   }
 
+  handleKeyUp(e) {
+    if (e.key === 'Enter') {
+      return
+    }
+  }
+
   render() {
     return(
       <Grid>
         <Row>
           <div className="search-wrapper">
-            <input className="input-search" placeholder="search movies"/>
+            <input className="input-search"
+                    placeholder="search movies"
+                    ref={input => this.search = input}
+                    onChange={this.handleInputChange}
+                    onKeyUp={this.handleKeyUp} />
           </div>
         </Row>
       </Grid>
