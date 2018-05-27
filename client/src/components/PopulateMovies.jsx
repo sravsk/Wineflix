47d15ccae90a04eb4b18dbb1e045ae50c6c00a36
Neiman Chen <hahatuke@gmail.com>
2018-05-27 14:55:42 -0700

    clean up

--- client/src/components/PopulateMovies.jsx
+++ client/src/components/PopulateMovies.jsx
@@ -3,38 +3,37 @@ import { Grid, Row, Col, Well, Thumbnail, Button} from 'react-bootstrap';
 
 const PopulateMovies = (props) => {
 
-    return(
-      <Grid>
-        {props.movies.map((movie) => (
-          <Row key={movie.id}>
-            <div>
-               <Col xs={6} md={4}>
-                  <Thumbnail src={`http://image.tmdb.org/t/p/original${movie.poster_path}`}/>
-                </Col>
-                <Col xs={6} md={4}>
-                  <div>{movie.title}</div>
-                  <Button>Suggest Wines</Button>
-                </Col>
-                <Col xs={6} md={4}>
-                  <div>Rating: {movie.vote_average}</div>
+  return(
+    <Grid>
+      {props.movies.map((movie) => (
+        <Row key={movie.id}>
+          <div>
+            <Col xs={6} md={4}>
+              <Thumbnail src={`http://image.tmdb.org/t/p/original${movie.poster_path}`}/>
+            </Col>
+            <Col xs={6} md={4}>
+              <div>{movie.title}</div>
+              <Button>Suggest Wines</Button>
+            </Col>
+            <Col xs={6} md={4}>
+              <div>Rating: {movie.vote_average}</div>
 
-                </Col>
-                <Col xs={6} md={4}>
-                  <div>Votes: {movie.vote_count}</div>
-                </Col>
-                <Col xs={6} md={4}>
-                  <div>Released: {movie.release_date}</div>
-                </Col>
-                <Col xs={6} md={4}>
-                  <br/>
-                  <Well bsSize="small">{movie.overview}</Well>
-                </Col>
-            </div>
-          </Row>
-        ))
-      }
-      </Grid>
-    )
+            </Col>
+            <Col xs={6} md={4}>
+              <div>Votes: {movie.vote_count}</div>
+            </Col>
+            <Col xs={6} md={4}>
+              <div>Released: {movie.release_date}</div>
+            </Col>
+            <Col xs={6} md={4}>
+              <br/>
+              <Well bsSize="small">{movie.overview}</Well>
+            </Col>
+          </div>
+        </Row>
+      ))}
+    </Grid>
+  )
 }
 
 export default PopulateMovies;
