d0c1f318c7e448c9536b8e1d2165b22b3b9c7d12
sravsk <sravanthi.karanam@gmail.com>
2018-05-28 13:47:18 -0700

    pass all wines data to IBM Waston, getting analysed data and saving it to db

--- services/winesFetcher.js
+++ services/winesFetcher.js
@@ -3,9 +3,9 @@ const dotenv = require('dotenv').config();
 const MongoDB = require('../database/index.js');
 
 
-let fetchWines = (data, cb) => {
+let fetchWines = (cb) => {
   let options = {
-    uri : 'https://lcboapi.com/products?q=wine&per_page=99&page=3&access_key=' + process.env.ACCESS_KEY,
+    uri : 'https://lcboapi.com/products?q=wine&per_page=99&page=35&access_key=' + process.env.ACCESS_KEY,
     headers : {
       'User-Agent' : 'Request-Promise'
     },
@@ -14,7 +14,8 @@ let fetchWines = (data, cb) => {
 
 requestPromise(options)
   .then((wines)=> {
-    MongoDB.saveWines(wines.result, cb);
+    MongoDB.saveWines(wines.result);
+    cb(wines);
   })
   .catch((err) => {
     console.log("cannot fetch data from LCBO API", err);
--- services/winesNLU.js
+++ services/winesNLU.js
@@ -2,28 +2,17 @@ const dotenv = require('dotenv').config();
 const MongoDB = require('../database/index.js');
 const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
-let winesNLU = (cb) => {
-  var nlu = new NaturalLanguageUnderstandingV1({
+var nlu = new NaturalLanguageUnderstandingV1({
   username: process.env.username,
   password: process.env.password,
-   version_date: '2018-04-05'
+   version_date: '2018-03-16'
  });
 
-
-
-  MongoDB.retriveWines()
-    .then((data) => {
-      for(var i = 0; i < data.length; i++) {
-      var keys = ['description', 'serving_suggestion', 'tasting_note', '_id'];
-
-      function obsKeysToString(obj, key, seperator) {
-        return key.map(key => obj[key]).filter(value => value).join(seperator);
-      }
-
-      var textToAnalyze = obsKeysToString(data[i], keys);
-
-      var parameters = {
-          'text': textToAnalyze,
+function wastonNLUWines(id , wineText) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    var parameters = {
+          'text': wineText,
           'features': {
             'sentiment': {},
             'emotion' : {},
@@ -32,25 +21,56 @@ let winesNLU = (cb) => {
           }
         }
 
-        return new Promise((resolve) => {
+        nlu.analyze(parameters,  (err, response) => {
+          if(err) {
+            console.log("error", err);
+          } else {
+            MongoDB.updateWinesWithAnalyzedData(id, response.sentiment, response.keywords, response.entities, response.emotion).then((updateddata) => {
+              resolve(updateddata._id);
+            });
+          }
+        });
+      }, 2000);
+  });
+}
 
-          nlu.analyze(parameters,  (err, response) => {
-            if(err) {
-              console.log("error", err);
-            } else {
 
-             var documentID = textToAnalyze.slice(-24);
+let winesNLU = (cb) => {
+  var promises = [];
 
-              return MongoDB.updateWinesWithAnalyzedData(documentID, response.sentiment, response.keywords, response.entities, response.emotion)
-                .then((updateddata) => {
-                  //console.log("updated data", updateddata);
-                })
-                //reslove();
-            }
-          })
-        }).then(() => reslove());
+  MongoDB.retriveWines()
+    .then((data) => {
+      var i;
+      for(i = 0; i < data.length; i++) {
+        var keys = ['description', 'serving_suggestion', 'tasting_note', '_id'];
+
+        function obsKeysToString(obj, key, seperator) {
+          return key.map(key => obj[key]).filter(value => value).join(seperator);
+        }
+
+        var textToAnalyze = obsKeysToString(data[i], keys);
+
+        promises.push(wastonNLUWines(data[i]._id, textToAnalyze));
       }
-    });
+
+      if( i === data.length) {
+        cb(true);
+         console.log("All done");
+        // try {
+        //   Promise.all(promises)
+        //     .then((results) => {
+        //       cb(results);
+        //       console.log("All done", results);
+        //     })
+        //     .catch((err) => {
+        //       console.log("error inside promise all", err);
+        //     });
+        //   }
+        //   catch ( err) {
+        //     console.log("failed promise all ", err);
+        //   }
+         }
+      });
   }
 
 module.exports.winesNLU = winesNLU;
