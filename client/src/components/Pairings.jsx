873c4fc0465a53d46d83d9c4478ea304f617a23c
Zoey Ikemoto <zmi384@gmail.com>
2018-05-30 16:18:13 -0700

    pairings now use movie db data
    wine db isn't working, will revisit

--- client/src/components/Pairings.jsx
+++ client/src/components/Pairings.jsx
@@ -5,20 +5,34 @@ import PopulatePairings from './PopulatePairings.jsx';
 //import SidebarMovies from './SidebarMovies.jsx';
 import moviesData from '../data/moviesData.js';
 import winesData from '../data/winesData.js';
+import $ from 'jquery';
 
 class Pairings extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-     // movies : []
+     movies : []
     }
   }
 
+  componentDidMount() {
+    $.ajax({
+      type: 'GET',
+      url: '/moviedata',
+      contentType: 'application/json',
+      success: (data) => {
+        this.setState({movies: data});
+      },
+      error: (err) => {
+        console.error('Client movie fetch error: ', err);
+      }
+    });
+  }
   render() {
     return(
       <Grid>
         {/*<SidebarMovies movies={moviesData}/>*/}
-        <PopulatePairings movies={moviesData} wines={winesData}/>
+        <PopulatePairings movies={this.state.movies} wines={winesData}/>
       </Grid>
       )
   }
