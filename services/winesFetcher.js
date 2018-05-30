7ed8504de5aea1ae85e03c1f34fe3e1c10802034
sravsk <sravanthi.karanam@gmail.com>
2018-05-29 18:29:13 -0700

    refactor winesFetcher service to fetch data from API in one go and not timeout

--- services/winesFetcher.js
+++ services/winesFetcher.js
@@ -4,22 +4,28 @@ const MongoDB = require('../database/index.js');
 
 
 let fetchWines = (cb) => {
-  let options = {
-    uri : 'https://lcboapi.com/products?q=wine&per_page=99&page=35&access_key=' + process.env.ACCESS_KEY,
-    headers : {
-      'User-Agent' : 'Request-Promise'
-    },
-    json : true
-  };
+  var maxPage = 60
+  for(let i = 1; i < maxPage; i++) {
+    let options = {
+      uri : 'https://lcboapi.com/products?q=wine&per_page=99&page=' + i + '&access_key=' + process.env.ACCESS_KEY,
+      headers : {
+        'User-Agent' : 'Request-Promise'
+      },
+      json : true
+    };
 
-requestPromise(options)
-  .then((wines)=> {
-    MongoDB.saveWines(wines.result);
-    cb(wines);
-  })
-  .catch((err) => {
-    console.log("cannot fetch data from LCBO API", err);
-  });
+    requestPromise(options)
+    .then((wines)=> {
+      MongoDB.saveWines(wines.result);
+      cb(wines);
+    })
+    .catch((err) => {
+      console.log("cannot fetch data from LCBO API", err);
+    });
+    setTimeout(function(){
+      console.log('iteration no ' + i)
+    }, i);
+  }
 }
 
 module.exports.fetchWines = fetchWines;
