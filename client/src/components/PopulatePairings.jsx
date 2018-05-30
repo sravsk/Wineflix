2a8d23f1b1332b7352e24d511b83b9b19f8d6c34
Zoey Ikemoto <zmi384@gmail.com>
2018-05-30 16:07:26 -0700

    refactored populatepairings component

--- client/src/components/PopulatePairings.jsx
+++ client/src/components/PopulatePairings.jsx
@@ -4,31 +4,27 @@ import { Grid, Row, Col, Well, Thumbnail, Button} from 'react-bootstrap';
 class PopulatePairings extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      data : this.props.movies,
-      wines : this.props.wines,
-    };
   }
 
   render(){
     return(
       <Grid>
-        {this.state.data.map((movie, index) => (
+        {this.props.movies.map((movie, index) => (
           <Row key={movie.id}>
             <div>
                <Col xs={3} md={3}>
                   <Thumbnail src={`http://image.tmdb.org/t/p/original${movie.poster_path}`}/>
                 </Col>
                <Col xs={3} md={3}>
-                  <div class="pairing-info">
+                  <div className="pairing-info">
                     <h4>{movie.title}</h4>
                     <p>paired with:</p>
-                    <h4>{this.state.wines[index].name}</h4>
+                    <h4>{this.props.wines[index].name}</h4>
                     <h1>&#x1f44d;   &#x1f44e;</h1>
                   </div>
                 </Col>
                 <Col xs={3} md={3}>
-                  <Thumbnail src={this.state.wines[index].image_thumb_url}/>
+                  <Thumbnail src={this.props.wines[index].image_thumb_url}/>
                 </Col>
             </div>
           </Row>
